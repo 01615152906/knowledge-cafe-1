09766a6377966d353de3937945b51cb782d5cd3e
01615152906md khairujjaman <khairujjaman.me@gmail.com>
2025-07-06 13:02:40 +0600

    first commit

---
+++ src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,26 @@
+
+// import React from 'react';
+
+import { useEffect, useState } from "react";
+
+const Blogs = () => {
+
+const [blogs, setBlogs] = useState([]);
+useEffect(() =>{
+
+fetch('Blogs.json')
+.then(res => res.json())
+// .then(data => console.log(data))
+.then(data => setBlogs(data))
+
+
+},[])
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
